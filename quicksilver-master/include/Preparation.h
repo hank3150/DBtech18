b0f2511a3a8549aa1645246310bf60287563137b
FONTYS\875856 <georgianamg93@gmail.com>
2018-02-26 13:46:22 +0100

    Test preparation() code

---
+++ quicksilver-master/include/Preparation.h
@@ -0,0 +1,24 @@
+//
+// Created by 875856 on 2/25/2018.
+//
+
+#ifndef QUICKSILVER_PREPARATION_H
+#define QUICKSILVER_PREPARATION_H
+
+#endif //QUICKSILVER_PREPARATION_H
+
+#include <unordered_map>
+#include <unordered_set>
+#include <vector>
+#include <iostream>
+#include <regex>
+#include <fstream>
+#include "Pair.h"
+
+class Preparation {
+
+public:
+
+    std::vector<Pair> pair;
+
+};
--- quicksilver-master/src/SimpleEstimator.cpp
+++ quicksilver-master/src/SimpleEstimator.cpp
@@ -5,26 +5,47 @@
 #include "SimpleGraph.h"
 #include "SimpleEstimator.h"
 
+#include "Preparation.h"
+
 SimpleEstimator::SimpleEstimator(std::shared_ptr<SimpleGraph> &g){
 
     // works only with SimpleGraph
     graph = g;
 }
 
+//no need
+//std::vector<std::vector<std::vector<uint32_t>>> list;
+
 void SimpleEstimator::prepare() {
 
+    std::vector<Preparation> list;
+
     // do your prep here
-    //std::cout << "\nPREPARE!" << graph->adj[1].size()<< std::endl;
     for (int i=0;i<graph->adj.size();i++)
     {
-        std::cout << "\nPREPARE!" << graph->adj[i].data()<< std::endl;
+        //look only in the adj list, rev doubles list
+        //std::cout << "\nrow " << i << " size: "<< graph->adj[i].size()<< std::endl;
+        for(int j=0;j<graph->adj[i].size();j++)
+        {
+            //std::cout << "from "<< j+1 <<" with "<< graph->adj[i].data()[j].first<<" to "<< graph->adj[i].data()[j].second << std::endl;
+
+            // uint32_t from = j+1;
+            // uint32_t to = graph->adj[i].data()[j].second;
+            // uint32_t edgeLabel = graph->adj[i].data()[j].first;
+
+            list[graph->adj[i].data()[j].first].emplace_back(Preparation());
+        }
     }
+
+    //std::cout << "\nrow " << 0 << " size: "<< list[0][0][1]<< std::endl;
 }
 
 cardStat SimpleEstimator::estimate(RPQTree *q) {
 
     // perform your estimation here
     prepare();
+
+    std::cout << q->data;
 
 
 
