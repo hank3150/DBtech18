f77ba96e4923c39a89bc4fb8971bc370207ce40e
hank3150 <hank3150@users.noreply.github.com>
2018-03-08 12:37:16 +0100

    Move
    Moved Files

---
+++ include/SimpleEstimator.h
@@ -0,0 +1,28 @@
+//
+// Created by Nikolay Yakovets on 2018-02-01.
+//
+
+#ifndef QS_SIMPLEESTIMATOR_H
+#define QS_SIMPLEESTIMATOR_H
+
+#include "Estimator.h"
+#include "SimpleGraph.h"
+
+class SimpleEstimator : public Estimator {
+
+    std::shared_ptr<SimpleGraph> graph;
+
+public:
+    explicit SimpleEstimator(std::shared_ptr<SimpleGraph> &g);
+    ~SimpleEstimator() = default;
+
+    void prepare() override ;
+    cardStat estimate(RPQTree *q) override ;
+
+    std::shared_ptr<SimpleGraph> estimate_aux(RPQTree *q);
+    static void calculate(uint32_t label, bool inverse);
+
+};
+
+
+#endif //QS_SIMPLEESTIMATOR_H
