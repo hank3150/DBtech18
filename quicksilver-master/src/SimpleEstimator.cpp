7e8065526d450856d21845c88cfe786f49167eb9
hank3150 <hank3150@users.noreply.github.com>
2018-03-08 12:30:45 +0100

    Add files via upload

--- quicksilver-master/src/SimpleEstimator.cpp
+++ quicksilver-master/src/SimpleEstimator.cpp
@@ -4,13 +4,7 @@
 
 #include "SimpleGraph.h"
 #include "SimpleEstimator.h"
-
-#include <unordered_map>
-#include <unordered_set>
-#include <vector>
-#include <iostream>
-#include <regex>
-#include <fstream>
+#include "SimpleEvaluator.h"
 
 SimpleEstimator::SimpleEstimator(std::shared_ptr<SimpleGraph> &g){
 
@@ -18,82 +12,138 @@ SimpleEstimator::SimpleEstimator(std::shared_ptr<SimpleGraph> &g){
     graph = g;
 }
 
-std::map< uint32_t, std::vector<uint32_t> > map0;
-std::map< uint32_t, std::vector<uint32_t> > map1;
-std::map< uint32_t, std::vector<uint32_t> > map2;
-std::map< uint32_t, std::vector<uint32_t> > map3;
+std::unordered_set <uint32_t> x;
+std::vector< std::unordered_set <uint32_t> > inNode={x,x,x,x};
+std::vector< std::unordered_set <uint32_t> > outNode={x,x,x,x};
+std::vector <uint32_t> y;
+std::vector< std::vector <uint32_t> > inNodeTotal={y,y,y,y};
+std::vector< std::vector <uint32_t> > outNodeTotal={y,y,y,y};
+
+uint32_t pass=0;
 
 void SimpleEstimator::prepare() {
 
     // do your prep here
-
-    //to execute prepare() once
-    if(map0.size()==0) {
-
+    if(pass==0) {
         for (int i = 0; i < graph->adj.size(); i++) {
-            //look only in the adj list, rev doubles list
-
             for (int j = 0; j < graph->adj[i].size(); j++) {
 
-                //std::cout << "from "<< j+1 <<" with "<< graph->adj[i].data()[j].first<<" to "<< graph->adj[i].data()[j].second << std::endl;
+                int edge = graph->adj[i].data()[j].first;
+                int to = graph->adj[i].data()[j].second;
+                int from = j + 1;
 
-                uint32_t edge = graph->adj[i].data()[j].first;
-                uint32_t to = graph->adj[i].data()[j].second;
+                inNode[edge].insert(to);
+                outNode[edge].insert(from);
 
-                //use to update for each data point in dictionary
-                std::vector<uint32_t> list;
-
-                switch (edge) {
-                    case 0:
-                        //check if in map
-                        if(map0.find(j+1)==map0.end())
-                        {
-                            list.emplace_back(to);
-                            map0.insert(std::pair<uint32_t, std::vector<uint32_t>>(j+1, list));
-                        }else {
-                            map0[j+1].emplace_back(to);
-                        }
-                    case 1:
-                        //check if in map
-                        if(map1.find(j+1)==map1.end())
-                        {
-                            list.emplace_back(to);
-                            map1.insert(std::pair<uint32_t, std::vector<uint32_t>>(j+1, list));
-                        }else {
-                            map1[j+1].emplace_back(to);
-                        }
-                    case 2:
-                        //check if in map
-                        if(map2.find(j+1)==map2.end())
-                        {
-                            list.emplace_back(to);
-                            map2.insert(std::pair<uint32_t, std::vector<uint32_t>>(j+1, list));
-                        }else {
-                            map2[j+1].emplace_back(to);
-                        }
-                    case 3:
-                        //check if in map
-                        if(map3.find(j+1)==map3.end()) {
-                            list.emplace_back(to);
-                            map3.insert(std::pair<uint32_t, std::vector<uint32_t>>(j+1, list));
-                        }else {
-                            map3[j+1].emplace_back(to);
-                        }
-                }
+                inNodeTotal[edge].emplace_back(to);
+                outNodeTotal[edge].emplace_back(from);
             }
         }
+        pass=1;
+    }
+}
+
+uint32_t TR=0;
+uint32_t TS=0;
+uint32_t VR=0;
+uint32_t VS=0;
+uint32_t nrPases=0;
+uint32_t noOut=0;
+uint32_t noIn=0;
+
+void initialize()
+{
+    TR=0;
+    TS=0;
+    VR=0;
+    VS=0;
+    nrPases=0;
+    noOut=0;
+    noIn=0;
+}
+
+std::shared_ptr<SimpleGraph> SimpleEstimator::estimate_aux(RPQTree *q) {
+
+    // estimate according to the AST bottom-up
+
+
+
+    if(q->isLeaf()) {
+        // project out the label in the AST
+        std::regex directLabel (R"((\d+)\+)");
+        std::regex inverseLabel (R"((\d+)\-)");
+
+        std::smatch matches;
+
+        uint32_t label;
+        bool inverse;
+
+        if(std::regex_search(q->data, matches, directLabel)) {
+            label = (uint32_t) std::stoul(matches[1]);
+            inverse = false;
+        } else if(std::regex_search(q->data, matches, inverseLabel)) {
+            label = (uint32_t) std::stoul(matches[1]);
+            inverse = true;
+        } else {
+            std::cerr << "Label parsing failed!" << std::endl;
+            return nullptr;
+        }
+
+        SimpleEstimator::calculate(label, inverse);
+
     }
 
+    if(q->isConcat()) {
+
+        // estimate the children
+
+        auto leftGraph = SimpleEstimator::estimate_aux(q->left);
+        auto rightGraph = SimpleEstimator::estimate_aux(q->right);
+
+
+    }
+
+    return nullptr;
+}
+
+void SimpleEstimator::calculate(uint32_t labell, bool inverse) {
+
+    if(TR == 0) {
+        //first pass
+        TR=inNodeTotal[labell].size();
+        VR=inNode[labell].size();
+        noOut=VR;
+        TS=outNodeTotal[labell].size();
+        VS=outNode[labell].size();
+        noIn=VS;
+
+        nrPases=std::min(TR*(TS/VS), TS*(TR/VR));
+    }
+    else {
+        TR=TS;
+        VR=VS;
+        noOut=VS;
+
+        TS=inNodeTotal[labell].size();
+        VS=inNode[labell].size();
+        noIn=VS;
+
+        nrPases=std::min(TR*(TS/VS), TS*(TR/VR));
+    }
 }
 
 cardStat SimpleEstimator::estimate(RPQTree *q) {
 
     // perform your estimation here;
+    initialize();
+    auto res = estimate_aux(q);
+    //return SimpleEstimator::computeStats(res);
 
-    RPQTree current = *q;
-
-
-    return cardStat {nullptr, nullptr, nullptr};
+    return cardStat {noOut, nrPases, noIn};
 }
+
+
+
+
 
 
